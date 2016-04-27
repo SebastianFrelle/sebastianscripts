670cb27c0069343fc01b8ed02393fb5c743d8260
Sebastian Frelle Koch <sebastian.frelle@gmail.com>
2016-04-27 14:18:37 +0200

    create game testing

--- table_foosball/table_foosball_test.rb
+++ table_foosball/table_foosball_test.rb
@@ -8,4 +8,19 @@ class FoosballTest < Minitest::Test
     @simon = Player.new("Simon")
     @kenichi = Player.new("Kenichi")
   end
+
+  # Positive testing
+  def test_create_game_with_two_players
+    game = Game.create([@sebastian], [@simon], 10, 0)
+    assert_equal [@sebastian], game.winner
+  end
+
+  def test_create_game_with_multiple_players
+    game = Game.create([@sebastian, @daniel], [@simon, @kenichi], 10, 0)
+    assert_equal [@sebastian, @daniel], game.winner
+  end
+
+  def method_name
+    
+  end
 end
