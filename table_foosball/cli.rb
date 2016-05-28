50cb6431b665eef5b3da58ba579c3ad8d0bbc553
Sebastian Frelle Koch <sebastian.frelle@gmail.com>
2016-05-28 12:36:05 +0200

    deletion of games and players via cli implemented

--- table_foosball/cli.rb
+++ table_foosball/cli.rb
@@ -5,7 +5,7 @@ class TableFoosballCLI < Thor
 
 	desc "create_new_game", "creates a new game interactively"
 	long_desc <<-LONGDESC
-	`create_new_game` interactively takes the user through the process of creating a new game, asking for user input corresponding to each variable as it goes along.
+	create_new_game interactively takes the user through the process of creating a new game, asking for user input corresponding to each variable as it goes along.
 	LONGDESC
 	def create_new_game
 		side1 = ask("Players on side 1:")
@@ -27,9 +27,9 @@ class TableFoosballCLI < Thor
 		Game.games.each { |game| p game }
 	end
 
-	desc "delete_all_games", "delete all games from database"
+	desc "clear_games", "clear all games from database"
 	option :all, :type => :boolean
-	def delete_all_games
+	def clear_games
 		Game.clear
 	end
 
@@ -51,13 +51,10 @@ class TableFoosballCLI < Thor
 		Player.players.each { |player| p player }
 	end
 
-	# desc "delete_players", "deletes players"
-	# option :all, :type => :boolean
-	# option :names
-	# def delete_players
-	# 	if options[:all]
-	# 		Player.clear
-	# end
+	desc "clear_players", "clear all players"
+	def clear_players
+		Player.clear
+	end
 end
 
 TableFoosballCLI.start(ARGV)
